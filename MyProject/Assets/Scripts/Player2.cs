d50ad6d7af21455ed4b10347aaa4045a23dd8eef
7cmMonsterPrime <sashaegorov.07@mail.ru>
2025-05-19 19:05:56 +0300

    neEbychtopomenl

--- MyProject/Assets/Scripts/Player2.cs
+++ MyProject/Assets/Scripts/Player2.cs
@@ -4,20 +4,43 @@ using System.Collections.Generic;
 
 public class Player2 : MonoBehaviour
 {
+    public static Player2 Instance { get; private set; }
     [SerializeField] private float moveSpeed = 5f;
     private Rigidbody2D rb;
     private Player2InputActions player2InputActions;
+    private bool isRunning2 = false;
+    private float minMovingSpeed2 = 0.1f;
 
     private void Awake()
     {
+        Instance = this;
         rb = GetComponent<Rigidbody2D>();
 
     }
     private void FixedUpdate2()
     {
-        Vector2 input2Vector = GameInput2.Instanse.GetMovementVector2();
-        input2Vector = input2Vector.normalized;
-        rb.MovePosition(rb.position + input2Vector * (moveSpeed * Time.fixedDeltaTime));
+        HandleMovement();
 
+    }
+    private void HandleMovement()
+    {
+        Vector2 inputVector = GameInput.Instanse.GetMovementVector();
+        inputVector = inputVector.normalized;
+        rb.MovePosition(rb.position + inputVector * (moveSpeed * Time.fixedDeltaTime));
+        if (Mathf.Abs(inputVector.x) > minMovingSpeed2 || Mathf.Abs(inputVector.y) > minMovingSpeed2)
+        {
+            isRunning2 = true;
+
+        }
+        else
+        {
+            isRunning2 = false;
+        }
+
+
+    }
+    public bool IsRunning()
+    {
+        return isRunning2;
     }
 }
