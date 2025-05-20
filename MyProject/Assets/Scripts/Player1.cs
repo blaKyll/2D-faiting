e31aa776d20b15735aa35b74dbbfcb12e34fa8be
7cmMonsterPrime <sashaegorov.07@mail.ru>
2025-05-20 10:08:54 +0300

    nichegonesdelal

--- MyProject/Assets/Scripts/Player1.cs
+++ MyProject/Assets/Scripts/Player1.cs
@@ -1,43 +1,63 @@
 using UnityEngine;
-using System.Collections;
-using System.Collections.Generic;
 
 public class Player1 : MonoBehaviour
 {
     public static Player1 Instance { get; private set; }
+
+    [Header("Movement Settings")]
     [SerializeField] private float moveSpeed = 5f;
+    [SerializeField] private float minMovingSpeed = 0.1f;
+
+
     private Rigidbody2D rb;
-    private PlayerInputActions playerInputActions;
-    private float minMovingSpeed = 0.1f;
     private bool isRunning = false;
+    private bool isAttacking = false;
+
     private void Awake()
     {
+
         Instance = this;
         rb = GetComponent<Rigidbody2D>();
-       
     }
+
+    private void Update()
+    {
+        IsAttacking(); // Обработка ввода в Update
+    }
+
     private void FixedUpdate()
     {
-        HandleMovement();
-
+        HandleMovement(); // Физика в FixedUpdate
     }
-    private void HandleMovement() 
+
+    private void IsAttacking()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Debug.Log("attack");
+            isAttacking = true;
+        }
+        else
+        {
+            isAttacking = false;
+        }
+    }
+
+    public bool IsAttack()
+    {
+        return isAttacking;
+    }
+
+    private void HandleMovement()
     {
         Vector2 inputVector = GameInput.Instanse.GetMovementVector();
         inputVector = inputVector.normalized;
         rb.MovePosition(rb.position + inputVector * (moveSpeed * Time.fixedDeltaTime));
 
-        if(Mathf.Abs(inputVector.x)>minMovingSpeed || Mathf.Abs(inputVector.y) > minMovingSpeed)
-        {
-            isRunning = true;
-
-        }
-        else
-        {
-            isRunning=false;
-        }
-
+        isRunning = (Mathf.Abs(inputVector.x) > minMovingSpeed ||
+                    (Mathf.Abs(inputVector.y) > minMovingSpeed));
     }
+
     public bool IsRunning()
     {
         return isRunning;
--- MyProject/Assets/Scripts/PlayerVisual1.cs
+++ MyProject/Assets/Scripts/PlayerVisual1.cs
@@ -4,6 +4,7 @@ public class PlayerVisual1 : MonoBehaviour
 {
     private Animator animator;
     private const string IS_RUNNING= "IsRunning";
+    private const string IS_ATTACK = "IsAttack";
     private void Awake()
     {
         animator= GetComponent<Animator>();
@@ -11,5 +12,6 @@ public class PlayerVisual1 : MonoBehaviour
     private void Update()
     {
         animator.SetBool(IS_RUNNING, Player1.Instance.IsRunning());
+        animator.SetBool(IS_ATTACK, Player1.Instance.IsAttack());
     }
 }
